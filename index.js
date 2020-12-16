08c5266f07fe43407ed4dd4b142d9aae005d4104
mbove77 <mbove77@gmail.com>
2020-12-16 12:11:23 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-/**
- * @format
- */
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.hello}>Hola, Martin</Text>
+      </View>
+    );
+  }
+}
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  hello: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+});
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(
+  'MyReactNativeApp',
+  () => HelloWorld
+);
--- src/res/Colors.js
+++
@@ -1,9 +0,0 @@
-const Colors = {
-  blackPearl: "#20252c",
-  charade: "#272c35",
-  zircon: "#e0e0e0",
-  picton: "#3c6fc8",
-  carmine: "#ef6372",
-  white: "#fff"
-}
-export default Colors
